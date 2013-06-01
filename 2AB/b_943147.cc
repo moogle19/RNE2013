0bb89959437747f7473637ee57bbf18f380bc172
moogle19 <moogle.seidel@gmail.com>
2013-05-31 19:38:05 -0700

    kind of working

--- 2AB/b_943147.cc
+++ 2AB/b_943147.cc
@@ -9,6 +9,7 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
@@ -102,36 +103,73 @@ int main(int argc, char** argv) {
    
     // Create a packet sink on the star "hub" to receive these packets
     uint16_t port = 50000;
-    Address sinkLocalAddress (InetSocketAddress (Ipv4Address::GetAny (), port));
-    PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", sinkLocalAddress);
-    ApplicationContainer sinkApp = sinkHelper.Install (nServer);
-    sinkApp.Start (Seconds (1.0));
-    sinkApp.Stop (Seconds (10.0));
+    Address sinkLocalAddress (InetSocketAddress (iRouterToC0.GetAddress(0), port));
+    PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+    ApplicationContainer sinkApp = sinkHelper.Install (nClient0);
+    sinkApp.Start (Seconds (0.));
+    sinkApp.Stop (Seconds (100.));
 
+	Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (nServer, TcpSocketFactory::GetTypeId ()); //source at Server
+	
+    Address sinkLocalAddress1 (InetSocketAddress (iRouterToC1.GetAddress(0), port));
+    PacketSinkHelper sinkHelper1 ("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+    ApplicationContainer sinkApp1 = sinkHelper.Install (nClient1);
+    sinkApp1.Start (Seconds (5.));
+    sinkApp1.Stop (Seconds (100.));
+    
+	Address sinkLocalAddress2 (InetSocketAddress (iRouterToC2.GetAddress(0), port));
+    PacketSinkHelper sinkHelper2 ("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), port));
+    ApplicationContainer sinkApp2 = sinkHelper.Install (nClient2);
+    sinkApp2.Start (Seconds (10.));
+    sinkApp2.Stop (Seconds (100.));
 
+	/*Ptr<MyApp> app = CreateObject<MyApp> ();
+  	app->Setup (ns3TcpSocket, sinkLocalAddress, 1040, 100000, DataRate ("250Kbps"));
+  	nRouter->AddApplication (app);
+  	app->SetStartTime (Seconds (1.));
+  	app->SetStopTime (Seconds (100.));
+*/
 	// Create the OnOff applications to send TCP to the server
     OnOffHelper clientHelper ("ns3::TcpSocketFactory", Address ());
     clientHelper.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
     clientHelper.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+    
+	OnOffHelper clientHelper1 ("ns3::TcpSocketFactory", Address ());
+    clientHelper1.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+    clientHelper1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+    
+	OnOffHelper clientHelper2 ("ns3::TcpSocketFactory", Address ());
+    clientHelper2.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+    clientHelper2.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
    
     //normally wouldn't need a loop here but the server IP address is different
     //on each p2p subnet
     ApplicationContainer clientApps;
-
     AddressValue remoteAddress(InetSocketAddress (iRouterToC0.GetAddress (0), port));
     clientHelper.SetAttribute ("Remote", remoteAddress);
     clientApps.Add (clientHelper.Install (nServer));
-
     clientApps.Start (Seconds (1.0));
-    clientApps.Stop (Seconds (10.0));
-	
-	Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+    clientApps.Stop (Seconds (100.0));
+
+    ApplicationContainer clientApps1;
+    AddressValue remoteAddress1(InetSocketAddress (iRouterToC1.GetAddress (0), port));
+    clientHelper1.SetAttribute ("Remote", remoteAddress1);
+    clientApps1.Add (clientHelper1.Install (nServer));
+    clientApps1.Start (Seconds (5.0));
+    clientApps1.Stop (Seconds (100.0));
+    
+	ApplicationContainer clientApps2;
+    AddressValue remoteAddress2(InetSocketAddress (iRouterToC2.GetAddress (0), port));
+    clientHelper2.SetAttribute ("Remote", remoteAddress2);
+    clientApps2.Add (clientHelper2.Install (nServer));
+    clientApps2.Start (Seconds (10.0));
+    clientApps2.Stop (Seconds (100.0));
 
     AsciiTraceHelper ascii;
     ptp.EnableAsciiAll (ascii.CreateFileStream ("first.tr"));
     ptp.EnablePcapAll ("first");
 	
-	Simulator::Stop (Seconds (60));
+	Simulator::Stop (Seconds (100));
 	Simulator::Run ();
 	Simulator::Destroy ();
 
